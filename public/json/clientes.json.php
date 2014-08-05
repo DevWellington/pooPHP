5859a3df2f0cf70672722909673b4e20e8de1a15
= <devwellington@gmail.com>
2014-08-04 21:33:00 -0300

    Adapted front-end for new features

--- public/json/clientes.json.php
+++ public/json/clientes.json.php
@@ -1,22 +1,27 @@
 <?php
 
-require_once "../../src/classes/cliente.class.php";
-require_once "../../src/classes/populateData.class.php";
-require_once "../../src/lib/json.lib.php";
-
 header('Content-Type: application/json');
 
-$clientData = new PopulateDefaultData();
+require_once "../../src/lib/json.lib.php";
+require_once "../../src/fixture.php";
 
 // GETs
 $get = (isset($_GET['type'])) ? $_GET['type'] : null;
 $id = (isset($_GET['id'])) ? $_GET['id'] : null;
 $orderBy = (isset($_GET['orderBy'])) ? $_GET['orderBy'] : null;
 
+
 // Conditions
-if($get === 'id' && $id !== null)
-	echo Json::getJson($clientData->getArClientsFull()[$id]);
-elseif($get === 'listaClientes')
-	echo Json::getJSONOrderBy($clientData->getArListClients(), $orderBy);
-elseif ($get === null)
+if($get === 'id' && $id !== null){
+	echo Json::getJson($arClientesFull[$id]);
+
+} elseif($get === 'listaClientes'){
+
+	$arListaClientes = [];
+	foreach ($arClientesFull as $key => $value)
+		$arListaClientes[$key] = $value->nome;
+	
+	echo Json::getJSONOrderBy($arListaClientes, $orderBy);
+} elseif ($get === null){
 	echo null;
+}

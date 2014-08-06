510a1a0483e4e11a961832f2e026f509e8121bee
= <devwellington@gmail.com>
2014-08-05 21:43:55 -0300

    function autoload - php-fig.org/psr/psr-0/ - spl_autoload_register

--- src/autoload.php
+++ src/autoload.php
@@ -1,6 +1,18 @@
 <?php
 
-define('CLASS_DIR', 'src/');
-set_include_path(get_include_path().DIRECTORY_SEPARATOR.CLASS_DIR);
-spl_autoload_register();
+function autoload($className)
+{
+	$className = ltrim( $className, '\\' );
+	$fileName  = '';
 
+	if ($lastNsPos = strrpos($className, '\\')) {
+		$namespace = substr($className, 0, $lastNsPos );
+		$className = substr($className, $lastNsPos + 1 );
+		$fileName = str_replace( '\\', DIRECTORY_SEPARATOR, $namespace ) . DIRECTORY_SEPARATOR;
+	}
+
+	$fileName .= str_replace( '_', DIRECTORY_SEPARATOR, $className ) . '.php';
+	require $fileName;
+}
+
+spl_autoload_register('autoload');

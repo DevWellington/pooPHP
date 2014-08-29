47d74879bc0d209a0c22dcb56da467a6d5dab0aa
Wellington Ribeiro <DevWellington@gmail.com>
2014-08-29 12:30:36 -0300

    Telefone field changed to BigInt(20)

--- src/fixture.php
+++ src/fixture.php
@@ -12,7 +12,7 @@ $sql = "
       id int(11) NOT NULL AUTO_INCREMENT,
       nome varchar(50) DEFAULT NULL,
       endereco varchar(255) DEFAULT NULL,
-      telefone int(20) DEFAULT NULL,
+      telefone bigint(20) DEFAULT NULL,
       starts int(1) DEFAULT NULL,
       type varchar(2) DEFAULT NULL,
       PRIMARY KEY (id),
@@ -63,7 +63,7 @@ $clientePF1
     ->setNome('Wellington Ribeiro')
     ->setEndereco('Rua Oito, 25')
     ->setEnderecoCobranca('Rua Oito, 25')
-    ->setTelefone('3180808080')
+    ->setTelefone(3180808080)
     ->setCpf('000.000.333-03')
     ->setStarts('5')
 ;
@@ -74,8 +74,8 @@ $clientePF2
     ->setNome('Jose Maria')
     ->setEndereco('Rua Talvez, 22')
     ->setEnderecoCobranca('Rua Talvez, 22')
+    ->setTelefone(2222121234)
     ->setCpf('111.222.222-00')
-    ->setTelefone('2222121234')
     ->setStarts('4')
 ;
 
@@ -84,7 +84,7 @@ $clientePF3
     ->setNome('Maria Jose')
     ->setEndereco('Rua Ontem, 33')
     ->setEnderecoCobranca('Rua Ontem, 33')
-    ->setTelefone('3312341234')
+    ->setTelefone(3312341234)
     ->setCpf('333.333.000-00')
     ->setStarts('3')
 ;
@@ -94,7 +94,7 @@ $clientePF4
     ->setNome('Claudio Jose')
     ->setEndereco('Rua Algo, 44')
     ->setEnderecoCobranca('Rua Algo, 44')
-    ->setTelefone('4444441235')
+    ->setTelefone(4444441235)
     ->setCpf('444.222.444-00')
     ->setStarts('2')
 
@@ -105,8 +105,8 @@ $clientePF5
     ->setNome('Marcia')
     ->setEndereco('Rua Nao Vou, 55')
     ->setEnderecoCobranca('Rua Nao Vou, 55')
+    ->setTelefone(5555551234)
     ->setCpf('555.555.555-00')
-    ->setTelefone('5555551234')
     ->setStarts('3')
 ;
 
@@ -117,7 +117,7 @@ $clientePJ1
     ->setNome('Empresa 001')
     ->setEndereco('Rua Empresa 1, 25, Sala 011')
     ->setEnderecoCobranca('Av Empresa 1, 25, Sala 001')
-    ->setTelefone('3133330000')
+    ->setTelefone(3133330000)
     ->setCnpj('00.111.000/0001-01')
     ->setStarts('1')
 ;
@@ -128,7 +128,7 @@ $clientePJ2
     ->setNome('Empresa 002')
     ->setEndereco('Rua Empresa 2, 25, Sala 022')
     ->setEnderecoCobranca('Av Empresa 2, 25, Sala 002')
-    ->setTelefone('3133330000')
+    ->setTelefone(3133330000)
     ->setCnpj('00.222.000/0002-02')
     ->setStarts('4')
 ;
@@ -139,7 +139,7 @@ $clientePJ3
     ->setNome('Empresa 003')
     ->setEndereco('Rua Empresa 3, 35, Sala 033')
     ->setEnderecoCobranca('Av Empresa 3, 35, Sala 003')
-    ->setTelefone('3133330000')
+    ->setTelefone(3133330000)
     ->setCnpj('00.333.000/0003-03')
     ->setStarts('5')
 ;
@@ -150,7 +150,7 @@ $clientePJ4
     ->setNome('Empresa 004')
     ->setEndereco('Rua Empresa 4, 45, Sala 044')
     ->setEnderecoCobranca('Av Empresa 4, 45, Sala 004')
-    ->setTelefone('3133330000')
+    ->setTelefone(3133330000)
     ->setCnpj('00.444.000/0004-04')
     ->setStarts('4')
 ;
@@ -161,7 +161,7 @@ $clientePJ5
     ->setNome('Empresa 005')
     ->setEndereco('Rua Empresa 5, 55, Sala 055')
     ->setEnderecoCobranca('Av Empresa 5, 55, Sala 005')
-    ->setTelefone('3133330000')
+    ->setTelefone(3133330000)
     ->setCnpj('00.555.000/0005-05')
     ->setStarts('5')
 ;
